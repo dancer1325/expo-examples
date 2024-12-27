76dfb349dbef1c907c3a56c1d87669b8921d32cb
alfredo.toledano <alfredo.toledano@allianzdirect.es>
2024-12-27 16:39:41 +0100

    doc(blank): add view examples

--- blank/App.js
+++ blank/App.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import { View, Text } from "react-native";
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function App() {
+// ONLY 1! export default is allowed
+/*export default function App() {
   return (
     <View
       style={{
@@ -12,4 +15,18 @@ export default function App() {
       <Text>Universal React with Expo</Text>
     </View>
   );
-}
+}*/
+
+const ViewBoxesWithColorAndText = () => {
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{height: 100, flexDirection: 'row'}}>
+                <View style={{backgroundColor: 'blue', flex: 0.2}} />
+                <View style={{backgroundColor: 'red', flex: 0.4}} />
+                <Text>Hello World!</Text>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
+};
+
+export default ViewBoxesWithColorAndText;
